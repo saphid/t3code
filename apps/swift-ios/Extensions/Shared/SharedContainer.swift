1d8a78308f305ae1925669b38038740d661def6b
Alex Southwell <saphid@gmail.com>
2026-08-17 11:31:43 +1000

    chore(swift-ios): use supported Test URL scheme

--- apps/swift-ios/Extensions/Shared/SharedContainer.swift
+++ apps/swift-ios/Extensions/Shared/SharedContainer.swift
@@ -3,7 +3,7 @@ import Foundation
 enum T3SharedContainer {
     #if DEBUG
     static let appGroupID = "group.com.alxs.t3code.typed-swiftui.dev"
-    static let urlScheme = "t3code-swiftui-personal"
+    static let urlScheme = "t3code-swiftui"
     #else
     static let appGroupID = "group.com.t3tools.t3code.swiftui"
     static let urlScheme = "t3code-swiftui"
