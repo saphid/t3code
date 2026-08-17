9aea8531c73509ef65ad3cbdb6eadc8dc6c1a096
Alex Southwell <saphid@gmail.com>
2026-08-17 11:16:06 +1000

    chore(swift-ios): configure personal Test build 79

--- apps/swift-ios/Extensions/Shared/SharedContainer.swift
+++ apps/swift-ios/Extensions/Shared/SharedContainer.swift
@@ -2,8 +2,8 @@ import Foundation
 
 enum T3SharedContainer {
     #if DEBUG
-    static let appGroupID = "group.com.t3tools.t3code.swiftui.dev"
-    static let urlScheme = "t3code-swiftui-dev"
+    static let appGroupID = "group.com.alxs.t3code.typed-swiftui.dev"
+    static let urlScheme = "t3code-swiftui-personal"
     #else
     static let appGroupID = "group.com.t3tools.t3code.swiftui"
     static let urlScheme = "t3code-swiftui"
